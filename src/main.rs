897d9e75cddbc2799a23231a058c529d5e668b7f
Biagio-Vittorioso <unoqualsiasi404@icloud.com>
2026-03-29 16:37:00 +0200

    Initialize GTK application with UI and NFT modules

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
+mod nft;
+mod ui;
 
+use gtk4 as gtk;
+use gtk::prelude::*;
+
+fn main() {
+    let app = gtk::Application::builder()
+        .application_id("com.example.nftables-manager")
+        .build();
+
+    app.connect_activate(|app| {
+        ui::app_window::build_main_window(app);
+    });
+
+    app.run();
+}
